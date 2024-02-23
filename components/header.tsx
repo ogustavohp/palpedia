b1cab8bc810ef9a45e5f28670530032d840cfdf0
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-02-23 16:08:06 -0300

    create link for /breading route

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Typography } from './typography'
 import { ModeToggle } from './mode-toggle'
+import Link from 'next/link'
 
 export function Header() {
   return (
     <header className="mx-6 mt-6 flex justify-between">
-      <Typography variant="h1">Palpedia</Typography>
+      <div className="flex items-center justify-center flex-col sm:flex-row gap-4 lg:gap-20">
+        <Typography variant="h1">Palpedia</Typography>
+        <Link href={'/breading'}>
+          <Typography>Breading</Typography>
+        </Link>
+      </div>
       <ModeToggle />
     </header>
   )
