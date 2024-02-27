8825651621afe7fc6adb6a8d3b1b0ef8c98d7ce6
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-02-27 04:12:48 -0300

    finish changes in db

--- app/breading/page.tsx
+++ app/breading/page.tsx
@@ -1,5 +1,33 @@
 import { Typography } from '@/components/typography'
+import { palsBreading } from '@/db/db'
+
+const palsBreadingDb = palsBreading
 
 export default function Breading() {
-  return <Typography>GG</Typography>
+  function uniqueElements(arr: string[][]): string[] {
+    const uniqueElementsSet: Set<string> = new Set()
+
+    arr.forEach((subArr) => {
+      subArr.forEach((element) => {
+        uniqueElementsSet.add(element)
+      })
+    })
+    return Array.from(uniqueElementsSet)
+  }
+  // const printPals = palsBreadingDb.map(pal => )
+  return (
+    <>
+      {palsBreadingDb.map((pal, i) => {
+        const uniqueArray = uniqueElements(pal.parents)
+        return (
+          <>
+            <Typography key={i}>{pal.pal}</Typography>
+            <Typography key={i}>
+              [{uniqueArray.map((e) => `"${e}"`).join(', ')}],
+            </Typography>
+          </>
+        )
+      })}
+    </>
+  )
 }
