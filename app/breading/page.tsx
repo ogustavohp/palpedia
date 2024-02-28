9453fc88cf994c2f97e0a556dbf118947e9c7197
Gustavo Henrique <gustavohpereira1.6@gmail.com>
2024-02-28 08:41:58 -0300

    finish logic pal breeding

--- app/breading/page.tsx
+++ app/breading/page.tsx
@@ -1,33 +1,78 @@
 import { Typography } from '@/components/typography'
-import { palsBreading } from '@/db/db'
+import { breedingMap } from '@/db/db'
 
-const palsBreadingDb = palsBreading
+// const palsBreadingDb = palsBreading
+
+const breedingMapDb = breedingMap
 
 export default function Breading() {
-  function uniqueElements(arr: string[][]): string[] {
-    const uniqueElementsSet: Set<string> = new Set()
+  // function uniqueElements(arr: string[][]): string[] {
+  //   const uniqueElementsSet: Set<string> = new Set()
 
-    arr.forEach((subArr) => {
-      subArr.forEach((element) => {
-        uniqueElementsSet.add(element)
-      })
-    })
-    return Array.from(uniqueElementsSet)
+  //   arr.forEach((subArr) => {
+  //     subArr.forEach((element) => {
+  //       uniqueElementsSet.add(element)
+  //     })
+  //   })
+  //   return Array.from(uniqueElementsSet)
+  // }
+
+  function findBreedingCombination(
+    targetPal: string,
+    ancestorPal: string,
+    breedingMap,
+  ) {
+    if (!breedingMap[ancestorPal] || !breedingMap[targetPal]) {
+      return 'Pal não encontrado no mapa de breeding.'
+    }
+    const queue: [string, string[]][] = [[targetPal, []]]
+    const visited = new Set()
+
+    while (queue.length > 0) {
+      const nextItem = queue.shift()
+
+      if (!nextItem) break
+
+      const [currentPal, parents] = nextItem
+
+      visited.add(currentPal)
+      if (currentPal === ancestorPal) {
+        return parents
+      }
+
+      if (breedingMap[currentPal]) {
+        const possibleParents = breedingMap[currentPal]
+
+        for (const parentPair of possibleParents) {
+          const [parent1, parent2] = parentPair
+
+          if (!visited.has(parent1) && !visited.has(parent2)) {
+            queue.push([parent1, parents.concat([parentPair])])
+            queue.push([parent2, parents.concat([parentPair])])
+          }
+        }
+      }
+    }
+    return 'Não foi encontrada uma combinação de breeding para chegar ao pal especificado.'
   }
-  // const printPals = palsBreadingDb.map(pal => )
+
+  console.log(
+    findBreedingCombination('Frostallion Noct', 'Anubis', breedingMapDb),
+  )
   return (
-    <>
-      {palsBreadingDb.map((pal, i) => {
-        const uniqueArray = uniqueElements(pal.parents)
-        return (
-          <>
-            <Typography key={i}>{pal.pal}</Typography>
-            <Typography key={i}>
-              [{uniqueArray.map((e) => `"${e}"`).join(', ')}],
-            </Typography>
-          </>
-        )
-      })}
-    </>
+    // <>
+    //   {palsBreadingDb.map((pal, i) => {
+    //     const uniqueArray = uniqueElements(pal.parents)
+    //     return (
+    //       <>
+    //         <Typography key={i}>{pal.pal}</Typography>
+    //         <Typography key={i}>
+    //           [{uniqueArray.map((e) => `"${e}"`).join(', ')}],
+    //         </Typography>
+    //       </>
+    //     )
+    //   })}
+    // </>
+    <Typography>oi</Typography>
   )
 }
